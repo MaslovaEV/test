9d6d9fa454ceecc486554ba108684b9ac8454d96
MaslovaEV <maslova_ev@bw-sw.com>
2017-06-09 15:22:14 +0700

    Create conf.py

--- MyProject/source/conf.py
+++ MyProject/source/conf.py
@@ -117,16 +117,16 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-import gzzle_sphinx_theme
-html_theme = 'guzzle_sphinx_theme'
+
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
-extensions.append("guzzle_sphinx_theme")        
+
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = guzzle_sphinx_theme.html_theme_path()
+# html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
